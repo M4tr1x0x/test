ce32ff32ea0c0b73f918b1b80b65b567e6d4126b
Matrix <alexgzzf@hotmail.com>
2024-02-07 15:33:01 -0600

    aaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,30 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <table>
-  <thead>
-    <tr>
-      <th>Encabezado 1</th>
-      <th>Encabezado 2</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Dato 1</td>
-      <td>Dato 2</td>
-    </tr>
-    <tr>
-      <td>Dato 3</td>
-      <td>Dato 4</td>
-    </tr>
-  </tbody>
-</table>
+          <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>depto</th>
+          <th>caja</th>
+          <th>fecha</th>
+          <th>empleado_numero</th>
+          <th>empleado_nombre</th>
+          <th>discrepancia</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Dato 1</td>
+          <td>Dato 2</td>
+        </tr>
+        <tr>
+          <td>Dato 3</td>
+          <td>Dato 4</td>
+        </tr>
+      </tbody>
+    </table>
 
-
-      
     </main>
   );
 }
