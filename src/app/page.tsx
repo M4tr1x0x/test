030314e6e4d94f2f4082698a2c6e3ef0f95eeec0
Matrix <alexgzzf@hotmail.com>
2024-02-07 15:29:36 -0600

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,25 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.cell}>Contenido de mi div</div>
+      <table>
+  <thead>
+    <tr>
+      <th>Encabezado 1</th>
+      <th>Encabezado 2</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Dato 1</td>
+      <td>Dato 2</td>
+    </tr>
+    <tr>
+      <td>Dato 3</td>
+      <td>Dato 4</td>
+    </tr>
+  </tbody>
+</table>
+
 
       
     </main>
